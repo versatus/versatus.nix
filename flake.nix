8b4ab93ca7a877e8d09602ffddc12ef39ea79d74
eureka-cpu <github.eureka@gmail.com>
2024-02-08 20:11:29 -0600

    add protocol dev shell & documentation

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
-  description = "A Nix flake for the Versatus labs ecosystem";
+  description =
+    "A Nix flake for the Versatus labs ecosystem that builds Versatus binaries and provides
+    development environments for building on Versatus repositories for supported systems.";
 
-   inputs = {
-    pkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    # rust toolchain provider
-    fenix-rust = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "pkgs";
-    };
   };
 
   outputs = inputs: let
+    # Set of packages for managing Rust toolchain
+    fenix-rust = import (fetchTarball {
+      url = "https://github.com/nix-community/fenix/archive/main.tar.gz";
+      sha256 = "sha256:003pgd0fg9n8j1wz4yg00yfij0lmy8w47dyn4zaljxhqhfbfmbxp";
+    }) {};
+    rust-toolchain = (fenix-rust.fromToolchainFile { dir = ./.; });
+
+    # Updates the systems that Versatus binaries support
+    # and dev-shells can be built for
     flake-utils.supportedSystem = [
       "x86_64-linux"
       "x86_64-openbsd"
@@ -23,24 +29,51 @@
     ];
     flake-utils.eachSupportedSystem =
       inputs.flake-utils.lib.eachSystem flake-utils.supportedSystem;
-    pkgs = pkgs.legacyPackages.${flake-utils.eachSupportedSystem};
-    
-  in rec {
-    devShells.${flake-utils.eachSupportedSystem}.default =
-      pkgs.mkShell
-      {
-        buildInputs = [
-          pkgs.rustc
-          pkgs.rustup
-          pkgs.rustfmt
-        ];
 
+    # @Function
+    #
+    # @Input: `nixpkgs`
+    # @Output: Attribute set of dev-shells
+    mkDevShells = pkgs: rec {
+      protocol-dev = pkgs.mkShell {
+        name = "protocol-dev";
+        nativeBuildInputs = [
+          rust-toolchain
+          pkgs.pkg-config
+          pkgs.clang
+        ];
+        buildInputs = with pkgs; [
+          rocksdb
+          openssl.dev
+          libiconv
+        ] ++ lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.Security
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
         shellHook = ''
-        echo "let's see"
+          export LIBCLANG_PATH="${pkgs.libclang.lib}/lib";
+          export ROCKSDB_LIB_DIR="${pkgs.rocksdb}/lib";
         '';
       };
-  };
+
+      default = protocol-dev;
+    };
+
+    # @Function
+    #
+    # @Input: `currentSystem` specified by `supportedSystem`
+    # @Output: builds `devShell`s & Versatus packages 
+    mkOutput = system: let
+      pkgs = import inputs.nixpkgs { inherit system; };
+    in {
+      # TODO: add mkPackages as part of output
+      # packges = mkPackages pkgs;
+      devShells = mkDevShells pkgs;
+      formatter = pkgs.alejandra;
+    };
+
+    # The output for each system.
+    systemOutputs = flake-utils.eachSupportedSystem mkOutput;
+  in
+    systemOutputs // { inherit flake-utils; };
 }
-
-
-
