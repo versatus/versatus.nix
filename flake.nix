915117c47c4a623bb275bf0ee116b629f6f98538
eureka-cpu <github.eureka@gmail.com>
2024-02-09 19:47:44 -0800

    add rust overlay, wip abstract rust toolchain for cross compilation

--- flake.nix
+++ flake.nix
@@ -6,26 +6,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix-rust = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    versatus = {
+      url = "github:versatus/versatus";
+      flake = false;
+    };
   };
 
   outputs = inputs: let
-    # Set of packages for managing Rust toolchain
-    fenix-rust = import (fetchTarball {
-      url = "https://github.com/nix-community/fenix/archive/main.tar.gz";
-      sha256 = "sha256:003pgd0fg9n8j1wz4yg00yfij0lmy8w47dyn4zaljxhqhfbfmbxp";
-    }) {};
-    rust-toolchain = (fenix-rust.fromToolchainFile { dir = ./.; });
-
     # Updates the systems that Versatus binaries support
     # and dev-shells can be built for
     flake-utils.supportedSystem = [
       "x86_64-linux"
-      "x86_64-openbsd"
       "x86_64-darwin"
-      "x86_64-windows"
       "aarch64-linux"
       "aarch64-darwin"
-      "riscv64-linux"
     ];
     flake-utils.eachSupportedSystem =
       inputs.flake-utils.lib.eachSystem flake-utils.supportedSystem;
@@ -34,22 +32,26 @@
     #
     # @Input: `nixpkgs`
     # @Output: Attribute set of dev-shells
-    mkDevShells = pkgs: rec {
+    mkDevShells = pkgs: let
+      rust-toolchain = pkgs.fenix.fromToolchainFile {
+        file = inputs.versatus + "/rust-toolchain.toml";
+        sha256 = "sha256-SXRtAuO4IqNOQq+nLbrsDFbVk+3aVA8NNpSZsKlVH/8=";
+      };
+    in rec {
       protocol-dev = pkgs.mkShell {
         name = "protocol-dev";
-        nativeBuildInputs = [
+        buildInputs = [
           rust-toolchain
-          pkgs.pkg-config
-          pkgs.clang
-        ];
-        buildInputs = with pkgs; [
+        ] ++ (with pkgs; [
+          pkg-config
+          clang
           rocksdb
           openssl.dev
           libiconv
         ] ++ lib.optionals stdenv.isDarwin [
           darwin.apple_sdk.frameworks.Security
           darwin.apple_sdk.frameworks.SystemConfiguration
-        ];
+        ]);
         shellHook = ''
           export LIBCLANG_PATH="${pkgs.libclang.lib}/lib";
           export ROCKSDB_LIB_DIR="${pkgs.rocksdb}/lib";
@@ -64,10 +66,24 @@
     # @Input: `currentSystem` specified by `supportedSystem`
     # @Output: builds `devShell`s & Versatus packages 
     mkOutput = system: let
-      pkgs = import inputs.nixpkgs { inherit system; };
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ inputs.fenix-rust.overlays.default ];
+      };
     in {
       # TODO: add mkPackages as part of output
-      # packges = mkPackages pkgs;
+      # versa-pkgs = mkPackages pkgs;
+
+      # TODO: finalize cross platform rust-toolchain
+      # fenix = {
+      #   default = pkgs.callPackage ./rust-toolchain.nix {
+      #     versatus = inputs.versatus;
+      #     rust-toolchain = pkgs.fenix.fromToolchainFile {
+      #       file = inputs.versatus + "/rust-toolchain.toml";
+      #       sha256 = "sha256-SXRtAuO4IqNOQq+nLbrsDFbVk+3aVA8NNpSZsKlVH/8=";
+      #     };
+      #   };
+      # };
       devShells = mkDevShells pkgs;
       formatter = pkgs.alejandra;
     };
