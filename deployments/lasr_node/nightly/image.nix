337ba5820e26bcd607ce5831aed05eb0a59826ab
eureka-cpu <github.eureka@gmail.com>
2024-06-04 22:51:27 -0700

    working image

--- deployments/lasr_node/nightly/image.nix
+++ deployments/lasr_node/nightly/image.nix
@@ -1,9 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  # opts = import ./nightly-options.nix { inherit pkgs; };
+  nightly_options = import ./nightly-options.nix { inherit pkgs; };
   nixos_config = {
     imports = [ <nixpkgs/nixos/modules/virtualisation/digital-ocean-image.nix> ];
-    options.virtualisation.digitalOceanImage.configFile = ../../digital-ocean/config-file.nix;
-  };
+    virtualisation.digitalOceanImage.configFile = ../../digital-ocean/config-file.nix;
+  } // nightly_options;
 in
 (pkgs.nixos nixos_config).digitalOceanImage
