3af136c0a87fc11a1bf114d25a90f774ca8c6fd0
eureka-cpu <github.eureka@gmail.com>
2024-06-17 00:32:50 -0700

    add setup scripts

--- deployments/lasr_node/common.nix
+++ deployments/lasr_node/common.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 let
   # TODO: Pull dockerhub images declaratively.
-  # Pull the PD server image from dockerhub
+  # Pull the PD server image from dockerhub.
   # pd-image-tar = pkgs.dockerTools.pullImage {
   #   imageName = "pingcap/pd";
   #   imageDigest = "sha256:0e87d077d0fd92903e26a6ebeda633d6979380aac6fc76aa24c6a02d25a404f6";
@@ -9,8 +9,8 @@ let
   #   finalImageTag = "latest";
   #   finalImageName = "pd";
   # };
-  # Starts the placement driver server for TiKV
-  pd-server = pkgs.writeShellScriptBin "pd-server.sh" ''
+  # Starts the placement driver server for TiKV.
+  start-pd-server = pkgs.writeShellScriptBin "start-pd-server.sh" ''
     docker run -d --name pd-server --network host pingcap/pd:latest \
         --name="pd1" \
         --data-dir="/pd1" \
@@ -20,7 +20,7 @@ let
         --advertise-peer-urls="http://0.0.0.0:2380"
   '';
 
-  # Pull the TiKV server image from dockerhub
+  # Pull the TiKV server image from dockerhub.
   # tikv-image-tar = pkgs.dockerTools.pullImage {
   #   imageName = "pingcap/tikv";
   #   imageDigest = "sha256:e68889611930cc054acae5a46bee862c4078af246313b414c1e6c4671dceca63";
@@ -28,14 +28,65 @@ let
   #   finalImageTag = "latest";
   #   finalImageName = "tikv";
   # };
-  # Starts the TiKV server
-  tikv-server = pkgs.writeShellScriptBin "tikv-server.sh" ''
+  # Starts the TiKV server.
+  start-tikv-server = pkgs.writeShellScriptBin "start-tikv-server.sh" ''
     docker run -d --name tikv-server --network host pingcap/tikv:latest \
         --addr="127.0.0.1:20160" \
         --advertise-addr="127.0.0.1:20160" \
         --data-dir="/tikv" \
         --pd="http://127.0.0.1:2379"
   '';
+
+  # Starts kubo IPFS daemon.
+  start-ipfs = pkgs.writeTextFile {
+    name = "start-ipfs.sh";
+    text = ''
+      IPFS_PATH=/app/tmp/kubo ipfs daemon
+    '';
+    executable = true;
+    destination = "/app/bin/start-ipfs.sh";
+  };
+
+  # Starts the lasr_node from the release build.
+  start-lasr = pkgs.writeTextFile {
+    name = "start-lasr.sh";
+    text = ''
+      PREFIX=/app/lasr
+      cd $PREFIX
+
+      # TODO: Figure out a way to pass the secret key securely.
+      # SECRET_KEY= \
+      BLOCKS_PROCESSED_PATH="/app/blocks_processed.dat" \
+      ETH_RPC_URL="https://u0anlnjcq5:xPYLI9OMwxRqJZqhfgEiKMeGdpVjGduGKmMCNBsu46Y@u0auvfalma-u0j1mdxq0w-rpc.us0-aws.kaleido.io/" \
+      EO_CONTRACT_ADDRESS=0x563f0efeea703237b32ae7f66123b864f3e46a3c \
+      COMPUTE_RPC_URL=ws://localhost:9125 \
+      STORAGE_RPC_URL=ws://localhost:9126 \
+      BATCH_INTERVAL=180 \
+      	/app/lasr/target/release/lasr_node
+    '';
+    executable = true;
+    destination = "/app/bin/start-lasr.sh";
+  };
+
+  # Main process script. Re/starts the node and dependencies.
+  start-overmind = pkgs.writeTextFile {
+    name = "start-overmind.sh"; 
+    text = ''
+      OVERMIND_CAN_DIE=reset overmind start -D -N /app/Procfile
+    '';
+    executable = true;
+    destination = "/app/bin/start-overmind.sh";
+  };
+  # Overmind's configuration file.
+  # The destination path is automatically created.
+  procfile = pkgs.writeTextFile {
+    name = "Procfile";
+    text = ''
+      ipfs: /app/bin/start-ipfs.sh
+      lasr: sleep 5 && /app/bin/start-lasr.sh
+    '';
+    destination = "/app/Procfile";
+  };
 in
 {
   # Packages that will be available on the resulting NixOS system.
@@ -50,8 +101,12 @@ in
     overmind
     tmux
   ] ++ [
-    pd-server
-    tikv-server
+    start-pd-server
+    start-tikv-server
+    start-ipfs
+    start-lasr
+    start-overmind
+    procfile
   ];
 
   # Enable docker socket
